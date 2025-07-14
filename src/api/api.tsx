ae0742e9bae7ea709e02066e9da1548044f5d61c
Stephan Haak <info@stephan-haak.com>
2025-07-14 17:18:55 +0200

    define types in api.tsx, minor TS bugfixes in DeleteIcon.tsx

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,16 +1,20 @@
+type JokeObject = [];
 export async function getJoke() {
-  let newJoke = "";
-  newJoke = await fetch("https://witzapi.de/api/joke/?language=de");
-  let result = await newJoke.json();
-  return result[0].text;
+  let response: Response;
+  response = await fetch("https://witzapi.de/api/joke/?language=de");
+  const data = await response.json();
+  return data[0].text;
 }
 
-export function addToLocalStorage(jokesToSave) {
+export function addToLocalStorage(jokesToSave: JokeObject): void {
   localStorage.setItem("jokesList", JSON.stringify(jokesToSave));
 }
 
-export function getFromLocalStorage() {
+export function getFromLocalStorage(): JokeObject {
   const jokesToLoad = localStorage.getItem("jokesList");
+  if (!jokesToLoad) {
+    return [];
+  }
   const UnJSON = JSON.parse(jokesToLoad);
   return UnJSON;
 }
